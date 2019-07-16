498b47829f4f9fa161185bee78ec6b8496d76448
mercedesmathews <52977261+mercedesmathews@users.noreply.github.com>
2019-07-16 15:54:00 -0400

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,22 @@
+function bigImg(x) {
+x.style.height = "30%";
+x.style.width = "30%";
+}
+
+function normalImg(x) {
+  x.style.height = "20%";
+  x.style.width = "20%";
+}
+
+function showhidecontent(id)
+{
+  var element = document.getElementById(id);
+  if (element.style.display === "block") {
+  element.style.dusplay ="none";
+  document.getElementById("show/hide").innerHTML="Show More"
+  }
+else {
+  element.style.display = "block";
+  document.getElementById("show/hide").innerHTML = "Hide"
+  }
+}
